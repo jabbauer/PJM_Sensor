a8b43afa5eaecf52a6f99a5060c0bba6b75f2ad0
jabbauer <j.bauer@gmx.us>
2024-09-15 19:53:38 -0500

    Update const.py

--- custom_components/HA_PJM_Sensor/const.py
+++ custom_components/HA_PJM_Sensor/const.py
@@ -5,6 +5,16 @@ CONF_API_KEY = "api_key"
 CONF_UPDATE_FREQUENCY = "update_frequency"
 CONF_MONITORED_VARIABLES = "monitored_variables"
 
+# Sensor Types
+CONF_INSTANTANEOUS_ZONE_LOAD = 'instantaneous_zone_load'
+CONF_INSTANTANEOUS_TOTAL_LOAD = 'instantaneous_total_load'
+CONF_ZONE_LOAD_FORECAST = 'zone_load_forecast'
+CONF_TOTAL_LOAD_FORECAST = 'total_load_forecast'
+CONF_ZONE_SHORT_FORECAST = 'zone_short_forecast'
+CONF_TOTAL_SHORT_FORECAST = 'total_short_forecast'
+CONF_ZONAL_LMP_AVG = 'zonal_lmp_avg'
+CONF_ZONAL_LMP_5MIN = 'zonal_lmp_5min'
+
 # Define available zones
 AVAILABLE_ZONES = [
     'PJM-RTO', 'MID-ATL/APS', 'AECO', 'BGE', 'DPL', 'JCPL', 'METED',
@@ -37,4 +47,16 @@ ZONE_TO_PNODE_ID = {
     'DEOK': 124076095,
     'EKPC': 970242670,
     'OVEC': 1709725933
+}
+
+# Define sensor types and their units
+SENSOR_TYPES = {
+    CONF_INSTANTANEOUS_ZONE_LOAD: ["Zone Load", 'MW'],
+    CONF_INSTANTANEOUS_TOTAL_LOAD: ["PJM Total Load", 'MW'],
+    CONF_ZONE_LOAD_FORECAST: ["Load Forecast", 'MW'],
+    CONF_TOTAL_LOAD_FORECAST: ["PJM Total Load Forecast", 'MW'],
+    CONF_ZONE_SHORT_FORECAST: ["Zone 2HR Forecast", "MW"],
+    CONF_TOTAL_SHORT_FORECAST: ["PJM 2HR Forecast", "MW"],
+    CONF_ZONAL_LMP_AVG: ["Hourly Average Zonal LMP", '$/MWh'],
+    CONF_ZONAL_LMP_5MIN: ["5-min Zonal LMP", '$/MWh'],
 }
