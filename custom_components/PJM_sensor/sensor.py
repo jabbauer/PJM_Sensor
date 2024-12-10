67b3273f43e5ff278ce79bf62e9b368195ddd0e6
jabbauer <j.bauer@gmx.us>
2024-12-09 23:37:12 -0600

    Update sensor.py

--- custom_components/PJM_sensor/sensor.py
+++ custom_components/PJM_sensor/sensor.py
@@ -133,17 +133,9 @@ class PJMSensor(SensorEntity):
                     self._name += ' ' + f'{identifier}'
 
         self._attr_unique_id = f"pjm_{sensor_type}_{identifier}"
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-    
-    @property
-    def unique_id(self):
-        """Return a unique ID for the sensor."""
-        return self._unique_id
-
+        self._attr_name = self._name
+        self._attr_native_unit_of_measurement = self._unit_of_measurement
+        
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
@@ -153,11 +145,6 @@ class PJMSensor(SensorEntity):
     def native_value(self):
         """Return the state of the sensor."""
         return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
 
     @property
     def extra_state_attributes(self):
